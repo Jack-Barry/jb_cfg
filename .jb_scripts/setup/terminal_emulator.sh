7edf486bb53fdd35afe054624f607e462158465f
Jack Barry <Jack@Barry.onl>
2024-07-31 16:58:13 -0600

    feat: Add custom Kitty icon

--- .jb_scripts/setup/terminal_emulator.sh
+++ .jb_scripts/setup/terminal_emulator.sh
@@ -11,9 +11,16 @@
 # 'shift'+'cmd'+',' -> Reload config
 ############################################################
 if [ ! -d "/Applications/kitty.app/" ]; then
-	echo "Kitty not already installed, installing it..."
-	curl -L https://sw.kovidgoyal.net/kitty/installer.sh | sh /dev/stdin
-	echo "✅ Installed Kitty terminal emulator"
+  echo "Kitty not already installed, installing it..."
+  curl -L https://sw.kovidgoyal.net/kitty/installer.sh | sh /dev/stdin
+  echo "✅ Installed Kitty terminal emulator"
+
+  # Set and use custom icon for kitty
+  ICON_PATH="$HOME/.config/kitty/kitty-dark.icns"
+  kitty +runpy 'from kitty.fast_data_types import cocoa_set_app_icon; import sys; cocoa_set_app_icon(*sys.argv[1:]); print("OK")' "$ICON_PATH" /Applications/kitty.app
+  rm /var/folders/*/*/*/com.apple.dock.iconcache
+  killall Dock
+  echo "✅ Now using custom icon for Kitty"
 else
-	echo "✅ Kitty terminal emulator already installed"
+  echo "✅ Kitty terminal emulator already installed"
 fi
