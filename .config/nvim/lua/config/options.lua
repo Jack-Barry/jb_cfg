98c9539f3e72c6c5bcbc65f516d6befb0e07e788
Jack Barry <Jack@Barry.onl>
2025-02-02 08:27:19 -0700

    fix: Reload files when modified externally
    Helps especially with package.json when installing/uninstalling packages

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -23,3 +23,11 @@ vim.api.nvim_create_autocmd("BufReadPost", {
     vim.treesitter.stop()
   end,
 })
+
+-- auto-reload files when modified externally
+-- https://neovim.discourse.group/t/a-lua-based-auto-refresh-buffers-when-they-change-on-disk-function/2482/4
+vim.o.autoread = true
+vim.api.nvim_create_autocmd({ "BufEnter", "CursorHold", "CursorHoldI", "FocusGained" }, {
+  command = "if mode() != 'c' | checktime | endif",
+  pattern = { "*" },
+})
