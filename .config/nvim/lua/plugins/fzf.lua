078284390a3ea4ec6bdfcb7dddbc485af2366373
Jack Barry <Jack@Barry.onl>
2025-05-24 18:06:59 -0600

    fix: Cleaner live grep
    Removes package-lock.json and friends from livegrep when searching

--- .config/nvim/lua/plugins/fzf.lua
+++ .config/nvim/lua/plugins/fzf.lua
@@ -1,1 +1,12 @@
-return { "junegunn/fzf", build = "./install --bin" }
+return {
+  { "junegunn/fzf", build = "./install --bin" },
+  {
+    "fzf-lua",
+    opts = {
+      fzf_colors = true,
+      grep = {
+        rg_opts = "--sort-files --hidden --column --line-number --no-heading --color=always --smart-case -g '!{.git,node_modules}/*' -g '!{package-lock.json,yarn.lock}'",
+      },
+    },
+  },
+}
